f66ee0b590c01fe2fa21fe2196eb262b851d9e26
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-05 17:59:32 +0600

    Learning JavaScript

---
+++ app.js
@@ -0,0 +1,10 @@
+//====================JAVASCRIPT SCOPE START====================//
+var lang = "Bangla";
+function learn(topic){
+    var lang = topic;
+    console.log(`I'm learning ${lang}`);
+}
+
+learn('JavaScript');
+console.log(`I know ${lang}`);
+//====================JAVASCRIPT SCOPE ENDED====================//
