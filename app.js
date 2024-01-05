3194d0fa1fa16e634f9cbee9515275047161abe7
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-05 19:51:06 +0600

    js primvitive vs reference

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ function learn(topic){
     var lang = topic;
     console.log(`I'm learning ${lang}`);
 }
-
 learn('JavaScript');
 console.log(`I know ${lang}`);
 //====================JAVASCRIPT SCOPE ENDED====================//
+
+//====================PREMATIVE START====================//
+let a = 5;
+let b = 6;
+a = b;
+console.log(`A = ${a}, B: ${b}`);
+//====================PREMATIVE ENDED====================//
+
+//====================REFERANCE START====================//
+let aa = ["html", "css"];
+let bb = ["Js", "react"];
+bb = aa;
+aa.push("tailwind");
+console.log(`aa: ${aa} and bb: ${bb}`);
+//====================REFERANCE ENDED===================//
